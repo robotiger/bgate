513ff4dc0d083e0535eb31f098f0ac3496b17def
robotiger <palmov-a@ya.ru>
2020-10-30 12:52:04 +0400

    now

---
+++ bfreconf.py
@@ -0,0 +1,11 @@
+import uuid
+import shelve
+
+config=shelve.open("/home/bfg/bgate/config")
+#if not "macgate" in config:
+config["macgate"]="%012x"%uuid.getnode()
+config["uuid"]=uuid.uuid1()
+config["broker"]="192.168.31.204"
+config["brokerport"]=1883
+config["topic"]="BFG5"
+config.sync() 
---
+++ reconf.sh
@@ -0,0 +1,8 @@
+sudo systemctl stop bgate
+python3 bfreconf.py
+sudo systemctl start bgate
+sudo systemctl status bgate
+
+
+
+
