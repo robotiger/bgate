a1c926b9d781209b98cb321ebbbaa35e933d34f0
Alexey Palmov <palmov-a@ya.ru>
2021-08-12 21:07:08 +0400

    now

--- sb.py
+++ sb.py
@@ -52,107 +52,107 @@ def logi(s):
     print(s)
 
 
-    class BlAdvCoder:
-        
-        @staticmethod
-        def commandledencoder(mac,r,g,b,t,p,o):
-            ## используется для управления светодиодом бикона
-            ## формирует строку для отправки в блютус модуль.
-            ## входные параметры мак адрес '010203040506', яркость цветов r,g,b, длительность в секундах, период в милисекундах, время включения в милисекундах
-            try:
-                enc_data= struct.pack('>5s6s3B3h',b'\xab\xba\x0f\00\04',bytes.fromhex(mac),r,g,b,t,p,o)
-            except:
-                return None
-            crc=zlib.crc32(enc_data)&0xffff
-            #print(crc32.to_bytes().hex(' '))
-            return struct.pack('>20sH',enc_data,crc)
-        
-              
-        @staticmethod
-        def decode2(dpin):
-            #dpo={'macgate':config['macgate']}
-            
-            dp1=dict(zip(['mac','rssi','band'],struct.unpack('2x6s3x2b',dpin[:13])))
-            if len(dpin)==43: # from beacons
-                dp2=dict(zip(['mfg','uuid','cnt','ext','exd','txpower'],       struct.unpack('6s16sHBBb', dpin[16:])))
-                dp3=dict(zip(['mfg','cnt','type','uuid','ext','exd','txpower'],struct.unpack('5sH2s8sBib',dpin[16:41])))
-                if dp2['mfg'].hex()=='1aff4c000215': #apple beacon
-                    dpo={**dp1,**dp2}
-                if dp3['mfg'].hex()=='16ffb1bf00': #hitech beacon
-                    dpo={**dp1,**dp3}
-                for c in dpo:
-                    if c in ['mac','mfg','uuid']:
-                        dpo[c]=dpo[c].hex()
-                dpo['gate']=config['macgate']
-                dpo['raw']=dpin.hex()
-                return dpo
-                 
+class BlAdvCoder:
     
-        @staticmethod   
-        def aesdecode(dpin):
-            key= b'fkytbwpt69xsbna3'
-            salt=b'lrjk;rdfkdldkhcngfle45'
-            dlina,mfg,once,coded=struct.unpack('13xb3s4s%ds'%(dpin[13]-7),dpin)        
-    #        print(dlina,ffmfg.hex(),once.hex(),coded.hex())
-            if mfg.hex()=='ffb1bf':
-                nonce=hashlib.sha1(once+salt)
-                rcv =AES.new(key,AES.MODE_EAX,nonce=nonce.digest()).decrypt(coded)
-                crc,cfg,data=struct.unpack('Hh%ds'%(dlina-11),rcv)
-                crcc=zlib.crc32(data)&0xffff
-                if crc==crcc:
-                    return (cfg,data)
-                else:
-                    return (0,'')
-
-class makemesage:
-    def __init__(self):
-        self.key= b'fkytbwpt69xsbna3'
-        #           0123456789012345
-        self.salt=b'lrjk;rdfkdldkhcngfle45'
-    def message(self,cfg,data):
-        once=AES.new(self.key,AES.MODE_EAX).nonce
-        self.ronce=once[:4]
-        print(self.ronce.hex())
-        self.nonce=hashlib.sha1(self.ronce+self.salt)
-        print(self.nonce.hexdigest())
-        if len(data)>19:
+    @staticmethod
+    def commandledencoder(mac,r,g,b,t,p,o):
+        ## используется для управления светодиодом бикона
+        ## формирует строку для отправки в блютус модуль.
+        ## входные параметры мак адрес '010203040506', яркость цветов r,g,b, длительность в секундах, период в милисекундах, время включения в милисекундах
+        try:
+            enc_data= struct.pack('>5s6s3B3h',b'\xab\xba\x0f\00\04',bytes.fromhex(mac),r,g,b,t,p,o)
+        except:
             return None
-        mes=pack('2sh%ds'%len(data),once[:2],cfg,data)
-        coded,tag = AES.new(self.key,AES.MODE_EAX,nonce=self.nonce.digest()).encrypt_and_digest(mes) 
-        lc=len(coded)
-        lr=len(self.ronce)
-        print(coded.hex())
-        return pack('5B4s23s', lc+4+lr,lc+3+lr,0xff,0xb1,0xbf,self.ronce,coded+b'\x00'*(24-lc))
+        crc=zlib.crc32(enc_data)&0xffff
+        #print(crc32.to_bytes().hex(' '))
+        return struct.pack('>20sH',enc_data,crc)
+    
+          
+    @staticmethod
+    def decode2(dpin):
+        #dpo={'macgate':config['macgate']}
+        
+        dp1=dict(zip(['mac','rssi','band'],struct.unpack('2x6s3x2b',dpin[:13])))
+        if len(dpin)==43: # from beacons
+            dp2=dict(zip(['mfg','uuid','cnt','ext','exd','txpower'],       struct.unpack('6s16sHBBb', dpin[16:])))
+            dp3=dict(zip(['mfg','cnt','type','uuid','ext','exd','txpower'],struct.unpack('5sH2s8sBib',dpin[16:41])))
+            if dp2['mfg'].hex()=='1aff4c000215': #apple beacon
+                dpo={**dp1,**dp2}
+            if dp3['mfg'].hex()=='16ffb1bf00': #hitech beacon
+                dpo={**dp1,**dp3}
+            for c in dpo:
+                if c in ['mac','mfg','uuid']:
+                    dpo[c]=dpo[c].hex()
+            dpo['gate']=config['macgate']
+            dpo['raw']=dpin.hex()
+            return dpo
+             
 
-    def decode(self,mes):
-#16 ff b1 bf c3 79 4a 0e 04 fb e1 e4 03 59 40 db 75 1b dc 28 db b3 75 
-#00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22
-#len        !nonce      ! mes
-                        # once! cfg ! data
-        dlina=mes[0]
-        ffmfg=mes[1:4]
-        once=mes[4:8]
-        coded=mes[8:dlina+1]
-        print(dlina,ffmfg.hex(),once.hex(),coded.hex())
-        if ffmfg.hex()=='ffb1bf':
-            nonce=hashlib.sha1(once+self.salt)
-            rcv =AES.new(self.key,AES.MODE_EAX,nonce=nonce.digest()).decrypt(coded)
-            ronce,cfg,data=unpack('2sh%ds'%(dlina-11),rcv)
-            print(ronce==once[:2],cfg,data)
+    @staticmethod   
+    def aesdecode(dpin):
+        key= b'fkytbwpt69xsbna3'
+        salt=b'lrjk;rdfkdldkhcngfle45'
+        dlina,mfg,once,coded=struct.unpack('13xb3s4s%ds'%(dpin[13]-7),dpin)        
+#        print(dlina,ffmfg.hex(),once.hex(),coded.hex())
+        if mfg.hex()=='ffb1bf':
+            nonce=hashlib.sha1(once+salt)
+            rcv =AES.new(key,AES.MODE_EAX,nonce=nonce.digest()).decrypt(coded)
+            crc,cfg,data=struct.unpack('Hh%ds'%(dlina-11),rcv)
+            crcc=zlib.crc32(data)&0xffff
+            if crc==crcc:
+                return (cfg,data)
+            else:
+                return (0,'')
 
-def decode(mes):
-    key= b'fkytbwpt69xsbna3'
-    salt=b'lrjk;rdfkdldkhcngfle45'
-    dlina=mes[0]
-    ffmfg=mes[1:4]
-    once=mes[4:8]
-    coded=mes[8:dlina+1]
-    print(dlina,ffmfg.hex(),once.hex(),coded.hex())
-    if ffmfg.hex()=='ffb1bf':
-        nonce=hashlib.sha1(once+salt)
-        rcv =AES.new(key,AES.MODE_EAX,nonce=nonce.digest()).decrypt(coded)
-        ronce,cfg,data=unpack('2sh%ds'%(dlina-11),rcv)
-        print(ronce==once[:2],cfg,data)
+#class makemesage:
+    #def __init__(self):
+        #self.key= b'fkytbwpt69xsbna3'
+        ##           0123456789012345
+        #self.salt=b'lrjk;rdfkdldkhcngfle45'
+    #def message(self,cfg,data):
+        #once=AES.new(self.key,AES.MODE_EAX).nonce
+        #self.ronce=once[:4]
+        #print(self.ronce.hex())
+        #self.nonce=hashlib.sha1(self.ronce+self.salt)
+        #print(self.nonce.hexdigest())
+        #if len(data)>19:
+            #return None
+        #mes=pack('2sh%ds'%len(data),once[:2],cfg,data)
+        #coded,tag = AES.new(self.key,AES.MODE_EAX,nonce=self.nonce.digest()).encrypt_and_digest(mes) 
+        #lc=len(coded)
+        #lr=len(self.ronce)
+        #print(coded.hex())
+        #return pack('5B4s23s', lc+4+lr,lc+3+lr,0xff,0xb1,0xbf,self.ronce,coded+b'\x00'*(24-lc))
+
+    #def decode(self,mes):
+##16 ff b1 bf c3 79 4a 0e 04 fb e1 e4 03 59 40 db 75 1b dc 28 db b3 75 
+##00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22
+##len        !nonce      ! mes
+                        ## once! cfg ! data
+        #dlina=mes[0]
+        #ffmfg=mes[1:4]
+        #once=mes[4:8]
+        #coded=mes[8:dlina+1]
+        #print(dlina,ffmfg.hex(),once.hex(),coded.hex())
+        #if ffmfg.hex()=='ffb1bf':
+            #nonce=hashlib.sha1(once+self.salt)
+            #rcv =AES.new(self.key,AES.MODE_EAX,nonce=nonce.digest()).decrypt(coded)
+            #ronce,cfg,data=unpack('2sh%ds'%(dlina-11),rcv)
+            #print(ronce==once[:2],cfg,data)
+
+#def decode(mes):
+    #key= b'fkytbwpt69xsbna3'
+    #salt=b'lrjk;rdfkdldkhcngfle45'
+    #dlina=mes[0]
+    #ffmfg=mes[1:4]
+    #once=mes[4:8]
+    #coded=mes[8:dlina+1]
+    #print(dlina,ffmfg.hex(),once.hex(),coded.hex())
+    #if ffmfg.hex()=='ffb1bf':
+        #nonce=hashlib.sha1(once+salt)
+        #rcv =AES.new(key,AES.MODE_EAX,nonce=nonce.digest()).decrypt(coded)
+        #ronce,cfg,data=unpack('2sh%ds'%(dlina-11),rcv)
+        #print(ronce==once[:2],cfg,data)
 
 
 class SerialBgate(threading.Thread):
