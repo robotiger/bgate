297e8e20afb92b26eee4b7dc44ec8bc4c2bf27de
Alexey Palmov <palmov-a@ya.ru>
2021-08-25 21:09:42 +0400

    now

--- bfgate.py
+++ bfgate.py
@@ -48,67 +48,6 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-
-"""
-   00 03 9c 97 89 7e 7c a8 01 ff 7f a9 25 02 01 1a 1b ff 8f 03 16 01 01 00 48 00 e4 3c 74 20 55 97 9c 89 a8 7c 7e 36 97 9c 89 a8 7c 7e   
-
-HIBEACON ble4
-   00 03 62 66 dc c1 de fb 01 ff 7f bc 25 02 01 06 1a ff 4c 00 02 15 00 00 00 01 7f 48 92 db f5 8c 11 9d c0 98 49 b7 01 64 b2 8a f8
-   00 03 62 66 dc c1 de fb 01 ff 7f d7 26 02 01 06 1a ff 4c 00 02 15 00 00 00 01 7f 48 92 db f5 8c 11 9d c0 98 49 b7 05 02 2a c7 f8  
-   00 03 11 e7 bb 3e 29 7b 01 ff 7f be 25 02 01 06 1a ff 4c 00 02 15 00 00 00 01 76 7d 25 88 86 36 5b b1 fa 2f 31 41 04 07 00 2b f8
--1 0  1  2  3   4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 
-   type | mac             |bits |tx|rs|pr|flags   |le|us|idmfg|id?  |     UUID                                      |major|minor|tx
-   const| id              |const   |var  |const   | const           |       id                                      |var                
-                                                               cccccccccccccccccrrrrrrrrrrrrccccccccccccccccccccc    
-
-HB ble5 
-   00 03 95 39 6e 74 da 05 01 ff 7f c0 27 02 01 06 16 ff b1 bf 00 1c 5f 01 00 95 39 6e 74 da 05 00 00 00 00 00 f8 72 d3 03 08 48 42 
-43 00 03 af bf cf df ef ff 01 ff 7f f2 26 02 01 06 16 ff bf b1 00 00 7d 01 00 ff ff ff ff ff ff 00 00 00 00 00 f8 42 c4 03 08 48 42
-   00 03 41 92 11 c2 a3 4c 01 ff 7f c1 26 02 01 06 16 ff b1 bf 00 00 0b 01 00 41 92 11 c2 a3 4c 00 00 00 00 00 f8 41 ab 03 08 48 42 
-   00 03 a4 59 b0 20 45 81 01 ff 7f ca 26 02 01 06 16 ff b1 bf 00 00 0d 01 00 a4 59 b0 20 45 81 00 00 00 00 00 f8 4d b4 03 08 48 42   
--1 0  1  2  3   4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 
-   type | mac             |bits |tx|rs|pr|flags   |le|us|idmfg|pid|cnt |dev  + id              |ty|exdata     |tp|crc  |ln|st|name
-   const| id              |const   |var  |const   | const         |var | id                    |var              |crc  |const
- 
-                                                               cccccccccccccccccrrrrrrrrrrrrccccccccccccccccccccc    
-
-   00 03 fe 0e 9b 4b fa c1 01 ff 7f bf 25 0f 09 4d 53 42 65 61 63 6f 6e 43 6f 6e 66 69 67 03 19 00 00 02 01 05
-                                        !
-   
-len 44  change to 43
-0:type:00 03
-2:mac: af bf cf df ef ff
-8: 01 ff 
-10:txpower:7f 
-11:rssi: f2
-12:port: 26
-13-15: 02 01 06 длина тип флаги
-16: 17 длина
-17: ff пользовательский тип
-18-19: fe c0 ид производителя
-#! 11 длина exclude
-20! 00 ид пакета широковещательный
-21-22! 00 84 счетчик пакета
-23:! 01 тип устройства
-24:! 00 подтип
-25-30! ff ff ff ff ff ff ид
-31! 00 тип данных
-33-35! 00 00 00 00 данные    
-36! f8 мощность передачи  
-37! 57 контрольная сумма  
-38! 2b контрольная сумма 
-39 03 длина
-
-40 08 тип короткое имя
-41-42 48 42  HB
-"""
-
-
-
-
-
-
-
 def get_ip_address(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     return socket.inet_ntoa(fcntl.ioctl(
@@ -207,11 +146,15 @@ class bgmqtt(threading.Thread):
             print('publoop while')
             while self.running and self.isconnected:
 #            while self.isconnected and not self.stop_event.is_set():
-                data = self.queue.get()
-                ret=self.mqttclient.publish(data['topic'],data['msg'])
-                if ret[0]!=0:
-                    self.queue.put(data)
-                    logi('cant sent message to %s'%(data))
+                try:
+                    data = self.queue.get(timeout=1)
+                except:
+                    data = None
+                if not data is None:
+                    ret=self.mqttclient.publish(data['topic'],data['msg'])
+                    if ret[0]!=0:
+                        self.queue.put(data)
+                        logi('cant sent message to %s'%(data))
             self.disconnect()
             time.sleep(0.5)
             
