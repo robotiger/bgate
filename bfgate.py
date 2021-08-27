db47a16553a1e608df47523aa11058449328e7e8
Alexey Palmov <palmov-a@ya.ru>
2021-08-27 20:38:11 +0400

    now

--- bfgate.py
+++ bfgate.py
@@ -315,7 +315,7 @@ if __name__ == '__main__':
     config=bgconfig.Configuration(stop_event)
     config.configurate(700,'R1 r1') # моргаем красным пока включаемся
     
-    config.write(303,'bfgss')
+   
     
     config.print()
 
