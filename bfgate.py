2f53634e0060e66af7a64a1732bd4cf3e8a6864a
Alexey Palmov <palmov-a@ya.ru>
2021-08-30 15:40:12 +0400

    now

--- bfgate.py
+++ bfgate.py
@@ -268,6 +268,7 @@ class bgserial(threading.Thread):
                                 if len(self.datapack)==43: # все используемые адвертисинг пакеты 43 байта
                                     d=bgcoder.BlAdvCoder.decode2(self.datapack)
                                     d['gate']=config.read('macgate')
+                                    d['factory']=config.read('factory')
                                     topic=config.read('brokertopic')
                                     #publish
                                     ret=mqt.publish({'topic':topic,'msg':msgpack.packb(d,use_bin_type=True)})                            
