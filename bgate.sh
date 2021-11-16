4429f9eb5c4210eb6779e68ef0bee81fc08b9971
Alexey Palmov <palmov-a@ya.ru>
2021-11-16 12:18:38 +0400

    now

--- bgate.sh
+++ bgate.sh
@@ -1,14 +1,14 @@
 #git pull
-if [ -f /home/bfg/bgate/extcommand.sh ]; then 
-   rm /home/bfg/bgate/extcommand.sh
+if [ -f /home/bfg/bgate/ecmd ]; then 
+   rm /home/bfg/bgate/ecmd
 fi
 if [ ! -f /home/bfg/bgate/config.db ]; then 
   /home/bfg/bgate/startwifi.sh
 fi
 python3 bfgate.py
 if [ -f /home/bfg/bgate/extcommand.sh ]; then 
-   /home/bfg/bgate/extcommand.sh
-   rm /home/bfg/bgate/extcommand.sh
+   /home/bfg/bgate/ecmd
+   rm /home/bfg/bgate/ecmd
 fi
 
 
