8692f635a4c417818c67f49d7c5c4b9fda7102b1
bfg <ev3dev maker>
2022-07-22 16:25:35 +0400

    now

--- bfgate.py
+++ bfgate.py
@@ -364,7 +364,6 @@ if __name__ == '__main__':
     config=bgconfig.Configuration(stop_event)
     config.configurate(700,'R1 r1') # моргаем красным пока включаемся
     
-    ip=bgconfig.ip_addresses()
     
     config.print()
 
@@ -382,11 +381,12 @@ if __name__ == '__main__':
     while(not stop_event.is_set()):
         #print(threading.enumerate())
         time.sleep(15)
+        ip=bgconfig.ip_addresses()
         d={'mac': '000000000000', 'rssi': -20, 'band': 38, 'raw': b'00', 'mfg': 'bfg1', 'txpower': -8}        
         d['gate']=config.read('macgate')
         d['factory']=config.read('factory')
         d['timegate']=time.time()
-        d['ip']=bgconfig.ip_addresses()
+        d['ip']=ip
         zmqt.publish(msgpack.packb(d,use_bin_type=True))        
         
         #if mqt.isconnected:
